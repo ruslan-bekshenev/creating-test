17045821e0e9b0634f6143f6c030532fae7cffea
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-08-09 03:53:26 +0500

    create question, add nvmrc

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateQuestionDto } from './dto/create-question.dto';
+import { QuestionService } from './question.service';
 
 @Controller('question')
-export class QuestionController {}
+export class QuestionController {
+  constructor(private questionService: QuestionService) {}
+
+  @Post()
+  create(@Body() createQuestionDto: CreateQuestionDto) {
+    return this.questionService.create(createQuestionDto);
+  }
+
+  @Get()
+  getList() {
+    return this.questionService.getList();
+  }
+}
--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateQuestionDto } from './dto/create-question.dto';
+import { Question } from './question.entity';
 
 @Injectable()
-export class QuestionService {}
+export class QuestionService {
+  constructor(
+    @InjectRepository(Question)
+    private questionRepository: Repository<Question>,
+  ) {}
+
+  async create(createQuestionDto: CreateQuestionDto) {
+    const { question } = createQuestionDto;
+
+    const questionObj = this.questionRepository.create({ question });
+
+    await this.questionRepository.save(questionObj);
+
+    return questionObj;
+  }
+
+  async getList() {
+    return this.questionRepository.find();
+  }
+}
