030243dc69335fffd2e14d61bafaeb245b3b0a48
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-08-09 23:41:26 +0500

    feature add question answer service

--- src/question_answer/question_answer.service.ts
+++ src/question_answer/question_answer.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { QuestionAnswer } from './question_answer.entity';
 
 @Injectable()
-export class QuestionAnswerService {}
+export class QuestionAnswerService {
+  constructor(
+    @InjectRepository(QuestionAnswer)
+    private questionAnswerRepository: Repository<QuestionAnswer>,
+  ) {}
+  
+}
